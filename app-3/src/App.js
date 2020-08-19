282fa8f20eee755bc935783f28aacd0d49dfcaf8
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2020-08-19 17:40:26 -0500

    finished app-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      arr: ["understand onChange", "understand handlers", "understand this.state", "understand e.target.value"],
+      input: ''
+    }
+  }
+
+  handleChange(value) {
+    this.setState({ input: value })
+  }
+
+  render() {
+    const list = this.state.arr
+      .filter(e => this.state.input === '' || e.includes(this.state.input))
+      .map((e, index) => <h2 key={index}>{e}</h2>)
+    return (
+
+      <div className="App">
+        <input className="input" type="text" onChange={(e) => this.handleChange(e.target.value)} />
+        {list}
+      </div>
+    )
+  }
 }
-
-export default App;
